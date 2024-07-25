2ef634a6a0d73e6884da49b1c1c6b582bdfc6a9f
jesus <Jesusgarba@gmail.com>
2024-07-25 19:10:00 +0200

    clean the code

--- app/src/main/java/com/example/navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -66,18 +63,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationComposeTheme {
-        Greeting("Android")
-    }
-}
